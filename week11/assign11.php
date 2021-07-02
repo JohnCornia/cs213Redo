723837e4581a34e4e4537a52e8c81456b34e2133
John Cornia <john.cornia.1@gmail.com>
2021-07-02 16:37:22 -0600

    php coding

--- week11/assign11.php
+++ week11/assign11.php
@@ -4,21 +4,52 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Confirmation Page</title>
+   <title>Purchase Confirmation Page</title>
    <link rel="stylesheet" type="text/css" href="./stylesheet.css">
 </head>
 <body>
+<form action="assign11_a.php" method="GET">
+<fieldset>
+         <legend>
+            <h1>Archery Store Checkout</h1>
+         </legend>
+<?php 
+$firstName = $_GET["first_name"]; 
+$lastName = $_GET["last_name"];
+$address = $_GET["address"];
+$phoneNumber = $_GET["phone"];
+$total = $_GET["total"];
+$expirationDate = $_GET["exp_date"];
 
-<?php $firstName = $_GET["first_name"]; 
+echo "First Name: ";
 echo $firstName;
+echo "<br>Last Name: ";
+echo $lastName;
+echo "<br>Address: ";
+echo $address;
+echo "<br>Phone: ";
+echo $phoneNumber;
+echo "<br>Chosen Items: <br>";
 if(!empty($_GET['items'])) {
 
     foreach($_GET['items'] as $value){
-        echo "Chosen Items : ".$value.'<br/>';
+        echo $value.'<br/>';
     }
-
 }
+echo "<br>Credit Card: <br>";
+if(!empty($_GET['card'])) {
+
+    foreach($_GET['card'] as $value){
+        echo $value.'<br/>';
+    }
+}
+echo "<br>Expiration Date: ";
+echo $expirationDate;
 
 ?>
+<input type="submit" name="submission" value="confirm">
+<input type="submit" name="submission" value="cancel">
+</fieldset>
+</form>
 </body>
 </html>
