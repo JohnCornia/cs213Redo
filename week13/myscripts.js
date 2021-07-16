ccb64ad9c3bceb9d0755cbd97fa884d272c733a0
John Cornia <john.cornia.1@gmail.com>
2021-07-16 01:18:09 -0600

    this should work

--- week13/myscripts.js
+++ week13/myscripts.js
@@ -1,161 +1,97 @@
-/*var error = document.getElementById("errorMessage");
 
-function validateFirstName() {
-    var firstName = document.getElementById("first_name");
-    if (firstName.value == "") {
-        firstName.focus();
-        error.innerHTML = "First Name not valid"
-        return false;
+
+function buildGetRequest() {
+    var performance = document.getElementsByName("performance");
+    var first_name_1 = document.getElementsByName("first_name_1");
+    var last_name_1 = document.getElementsByName("last_name_1");
+    var student_id_1 = document.getElementsByName("student_id_1");
+    var first_name_2 = document.getElementsByName("first_name_2");
+    var last_name_2 = document.getElementsByName("last_name_2");
+    var student_id_2 = document.getElementsByName("student_id_2");
+    var skill = document.getElementsByName("skill");
+    var instrument = document.getElementsByName("instrument");
+    var location = document.getElementsByName("location");
+    var room = document.getElementsByName("room");
+    var time_slot = document.getElementsByName("time_slot");
+    var getRequestString = "performance=" + performance[0].value + "&first_name_1=" + first_name_1[0].value + "&last_name_1=" + last_name_1[0].value + "&student_id_1=" + student_id_1[0].value + "&first_name_2=" + first_name_2[0].value + "&last_name_2=" + last_name_2[0].value + "&student_id_2=" + student_id_2[0].value + "&skill=" + skill[0].value + "&instrument=" + instrument[0].value + "&location=" + location[0].value + "&room=" + room[0].value + "&time_slot=" + time_slot[0].value;
+    
+    return getRequestString;
+
+}
+
+function isDuet() {
+    var performance = document.getElementsByName("performance");
+    var first_name_2 = document.getElementsByName("first_name_2");
+    var last_name_2 = document.getElementsByName("last_name_2");
+    var student_id_2 = document.getElementsByName("student_id_2");
+
+    if (performance[0].value != "Duet") {
+        first_name_2[0].style.display = "none";
+        first_name_2[0].required = false;
+        last_name_2[0].style.display = "none";
+        last_name_2[0].required = false;
+        student_id_2[0].style.display = "none";
+        student_id_2[0].required = false;
     }
     else {
-        error.innerHTML = "";
-        return true;
+
+        first_name_2[0].style.display = "inline";
+        first_name_2[0].required = true;
+        last_name_2[0].style.display = "inline";
+        last_name_2[0].required = true;
+        student_id_2[0].style.display = "inline";
+        student_id_2[0].required = true;
     }
 }
-
-function validateLastName() {
-    var lastName = document.getElementById("last_name");
-    if (lastName.value == "") {
-        lastName.focus();
-        error.innerHTML = "Last Name not valid"
-        return false;
-    }
-    else {
-        error.innerHTML = "";
-        return true;
-    }
-}
-
-function validateAddress() {
-    var address = document.getElementById("address");
-    if (address.value == "") {
-        address.focus();
-        error.innerHTML = "Address not valid"
-        return false;
-    }
-    else {
-        error.innerHTML = "";
-        return true;
-    }
-}
-
-function validatePhone() {
-    var phoneNumber = document.getElementById("phone");
-    var pattern = /(\d{3})-(\d{3})-(\d{4})/g;
-    if (pattern.test(phoneNumber.value)) {
-        error.innerHTML = "";
-        return true;
-    }
-    else {
-        phoneNumber.focus();
-        error.innerHTML = "Phone Number not valid"
-        return false;
-    }
-}
-
-function cardName() {
-    var card = document.getElementsByName("card");
-    if (!card[0].checked && !card[1].checked && !card[2].checked) {
-        card[0].focus();
-        error.innerHTML = "Credit Card Company not selected"
-        return false;
-    }
-    else {
-        total.innerHTML = "";
-        return true;
-    }
-}
-
-function creditCard() {
-    var cardNumber = document.getElementById("credit_card");
-    var pattern = /(\d{4}\s){3}\d{4}/g;
-    if (pattern.test(cardNumber.value)) {
-        error.innerHTML = "";
-        return true;
-    }
-    else {
-        cardNumber.focus();
-        error.innerHTML = "Credit Card Number not valid"
-        return false;
-    }
-}
-
-function expirationDate() {
-    var expirationDate = document.getElementById("exp_date");
-    var pattern = /((([1][0-2]|[0][7-9])\/[2][0][2][1])|(([1][0-2]|[0][1-9])\/[2][0]([2][2-9]|[3-9][0-9])))/g;
-    if (pattern.test(expirationDate.value)) {
-        error.innerHTML = "";
-        return true;
-    }
-    else {
-        expirationDate.focus();
-        error.innerHTML = "Expiration Date not valid"
-        return false;
-    }
-}
-
-function calculateTotal() {
-    var total = 0;
-    if (document.getElementById("item_0").checked) {
-        total += 80;
-    }
-    if (document.getElementById("item_1").checked) {
-        total += 15;
-    }
-    if (document.getElementById("item_2").checked) {
-        total += 25;
-    }
-    if (document.getElementById("item_3").checked) {
-        total += 60;
-    }
-
-    document.getElementById("total").innerHTML = "$" + total;
-}
-
-function validateAll() {
-
-    if (validateFirstName() &&
-        validateLastName() &&
-        validateAddress() &&
-        validatePhone() &&
-        creditCard() &&
-        cardName() &&
-        expirationDate()) {
-
-    }
-    else { }
-}*/
 
 function ajaxRequest() {
 
+    var performance = document.getElementsByName("performance");
+         var first_name_1 = document.getElementsByName("first_name");
+         //console.log(first_name_1[0].value);
+         var last_name_1 = document.getElementsByName("last_name");
+         var student_id_1 = document.getElementsByName("student_id");
+         var first_name_2 = document.getElementsByName("first_name_2");
+         var last_name_2 = document.getElementsByName("last_name_2");
+         var student_id_2 = document.getElementsByName("student_id_2");
+         var skill = document.getElementsByName("skill");
+         var instrument = document.getElementsByName("instrument");
+         var location = document.getElementsByName("location");
+         var room = document.getElementsByName("room");
+         var time_slot = document.getElementsByName("time_slot");
+         var getRequestString = "performance=" + performance[0].value + "&first_name="
+            + first_name_1[0].value + "&last_name=" + last_name_1[0].value + "&student_id="
+            + student_id_1[0].value + "&first_name_2=" + first_name_2[0].value + "&last_name_2="
+            + last_name_2[0].value + "&student_id_2=" + student_id_2[0].value + "&skill="
+            + skill[0].value + "&instrument=" + instrument[0].value + "&location="
+            + location[0].value + "&room=" + room[0].value + "&time_slot=" + time_slot[0].value;
+
+    //console.log(getRequestString);
     const xhttp = new XMLHttpRequest();
-    xhttp.onload = function () {
-        const myObj = JSON.parse(this.responseText);
-        buildTable(myObj);
-    }
-    xhttp.open("GET", "./data/data.json", true);
+    console.log(getRequestString);
+
+    xhttp.open("GET", "./assign13.php?" + getRequestString, true);
     xhttp.send();
 }
 
-function buildTable(myObj) {
+function buildTable() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function () {
+        const myObj = JSON.parse(this.responseText);
+        var tableString = "<tr><th>Performance</th><th>First Name</th><th>Last Name</th><th>Student ID</th><th>Second Student First Name</th><th>Second Student Last Name</th><th>Second Student Student ID</th><th>Skill</th><th>Instrument</th><th>Location</th><th>Room</th><th>Time Slot</th></tr>";
+        for (let i = 0; i < myObj.length; i++) {
 
-    var tableString = "<tr><th>Performance</th><th>First Name</th><th>Last Name</th><th>Student ID</th><th>Second Student First Name</th><th>Second Student Last Name</th><th>Second Student Student ID</th><th>Skill</th><th>Instrument</th><th>Location</th><th>Room</th><th>Time Slot</th></tr>";
-    for (let i = 0; i < myObj.length; i++) {
+            tableString += "<tr><td>" + myObj[i].performance + "</td><td>" + myObj[i].first_name + "</td><td>" + myObj[i].last_name + "</td><td>" + myObj[i].student_id + "</td>";
 
-        /*if (myObj[i].performance == "Duet") {
-            tableString += "<th>Second Student First Name</th><th>Second Student Last Name</th><th>Second Student Student ID</th>";
-        }*/
+            tableString += "<td>" + myObj[i].first_name_2 + "</td><td>" + myObj[i].last_name_2 + "</td><td>" + myObj[i].student_id_2 + "</td>";
 
-        //tableString += "<th>Skill</th><th>Instrument</th><th>Location</th><th>Room</th><th>Time Slot</th></tr>"
-        tableString += "<tr><td>" + myObj[i].performance + "</td><td>" + myObj[i].first_name + "</td><td>" + myObj[i].last_name + "</td><td>" + myObj[i].student_id + "</td>";
-
-        //if (myObj[i].performance == "Duet") {
-        tableString += "<td>" + myObj[i].first_name_2 + "</td><td>" + myObj[i].last_name_2 + "</td><td>" + myObj[i].student_id_2 + "</td>";
-        //}
-
-        tableString += "<td>" + myObj[i].skill + "</td><td>" + myObj[i].instrument + "</td><td>" + myObj[i].location + "</td><td>" + myObj[i].room + "</td><td>" + myObj[i].time_slot + "</td></tr>";
+            tableString += "<td>" + myObj[i].skill + "</td><td>" + myObj[i].instrument + "</td><td>" + myObj[i].location + "</td><td>" + myObj[i].room + "</td><td>" + myObj[i].time_slot + "</td></tr>";
+        }
+        document.getElementById("registry").innerHTML = tableString;
     }
-    document.getElementById("registry").innerHTML = tableString;
+
+    xhttp.open("GET", "./data/data.json", true);
+    xhttp.send();
 
 }
 
